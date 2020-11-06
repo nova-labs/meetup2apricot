52e0d4a89bdb6dc85d820bc20e6e3ed5b8d9a513
Joel Shprentz <jshprentz@his.com>
2020-11-06 07:33:50 -0500

    Build paths to local and Wild Apricot photos.

--- meetup2apricot/photo_cache.py
+++ meetup2apricot/photo_cache.py
@@ -14,13 +14,13 @@ class PhotoCache:
         self.local_directory = local_directory
         self.apricot_directory = apricot_directory
 
-    def apricot_photo_path(self, meetup_event):
-        """Return the Wild Apricot photo path for a Meetup event or None."""
-        if meetup_event.photo_url:
-            photo_file_name = self.apricot_photo_file_name(meetup_event)
-            return self.apricot_directory / photo_file_name
-        else:
-            return None
+    def apricot_photo_path(self, photo_file_name):
+        """Return a photo's Wild Apricot URL path."""
+        return self.apricot_directory / photo_file_name
+
+    def local_photo_path(self, photo_file_name):
+        """Return a photo's local path."""
+        return self.local_directory / photo_file_name
 
     def apricot_photo_file_name(self, meetup_event):
         """Return the Wild Apricot photo file name for a Meetup event."""
--- tests/test_photo_cache.py
+++ tests/test_photo_cache.py
@@ -3,9 +3,17 @@
 from meetup2apricot.photo_cache import PhotoCache
 from meetup2apricot.meetup_event import MeetupEvent
 from datetime import datetime
+from pathlib import Path, PurePosixPath
 import pytest
 
 SAMPLE_DATE = datetime.fromisoformat("2020-11-09 18:30 -05:00")
+SAMPLE_APRICOT_DIRECTORY = PurePosixPath("/resources/photos")
+SAMPLE_LOCAL_DIRECTORY = Path("/var/tmp/photos")
+
+@pytest.fixture()
+def photo_cache():
+    """Return a photo cache."""
+    return PhotoCache(SAMPLE_LOCAL_DIRECTORY, SAMPLE_APRICOT_DIRECTORY)
 
 def test_apricot_photo_name_short():
     """Test converting a short Meetup name to a Wild Apricot photo name."""
@@ -23,11 +31,17 @@ def test_apricot_photo_name_very_long():
     expected_name = "EL_M_Soldering_Station_101_2020-11-09"
     assert PhotoCache.apricot_photo_name(long_name, SAMPLE_DATE) == expected_name
 
-def test_apricot_photo_file_name(free_meetup_event_json):
+def test_apricot_photo_file_name(free_meetup_event_json, photo_cache):
     """Test formatting a Wild Apricot photo file name for a Meetup event."""
     meetup_event = MeetupEvent(free_meetup_event_json)
-    photo_cache = PhotoCache("/foo", "/bar")
     assert photo_cache.apricot_photo_file_name(meetup_event) == "TEST_ETL_AC_Mending_Monday_2020-11-09.jpeg"
 
+def test_apricot_photo_path(photo_cache):
+    """Test building a path to a Wild Apricot photo."""
+    assert photo_cache.apricot_photo_path("photo.jpg") == PurePosixPath("/resources/photos/photo.jpg")
+
+def test_local_photo_path(photo_cache):
+    """Test building a path to a local photo."""
+    assert photo_cache.local_photo_path("photo.jpg") == Path("/var/tmp/photos/photo.jpg")
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 autoindent
