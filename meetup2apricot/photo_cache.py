b905d32ab3ccd9178715f30400c5cf4ff3653314
Joel Shprentz <jshprentz@his.com>
2020-11-06 07:16:15 -0500

    Wild Apricot photo file name uses extension from Meetup file name.

--- meetup2apricot/photo_cache.py
+++ meetup2apricot/photo_cache.py
@@ -1,6 +1,8 @@
 """Cache featured photos from Meetup events."""
 
 import re
+from urllib.parse import urlparse
+from pathlib import PurePosixPath
 
 
 class PhotoCache:
@@ -12,8 +14,24 @@ class PhotoCache:
         self.local_directory = local_directory
         self.apricot_directory = apricot_directory
 
+    def apricot_photo_path(self, meetup_event):
+        """Return the Wild Apricot photo path for a Meetup event or None."""
+        if meetup_event.photo_url:
+            photo_file_name = self.apricot_photo_file_name(meetup_event)
+            return self.apricot_directory / photo_file_name
+        else:
+            return None
+
+    def apricot_photo_file_name(self, meetup_event):
+        """Return the Wild Apricot photo file name for a Meetup event."""
+        file_name = self.apricot_photo_name(
+                meetup_event.name,
+                meetup_event.start_time)
+        extension = PurePosixPath(urlparse(meetup_event.photo_url).path).suffix
+        return file_name + extension
+
     @staticmethod
-    def name_for_apricot(name, date):
+    def apricot_photo_name(name, date):
         """Choose a descriptive photo name for Wild Apricot."""
         alpha_numeric_name = re.sub("[^A-Za-z0-9_]", "_", name + "_")
         tighter_name = re.sub("__+", "_", alpha_numeric_name)
