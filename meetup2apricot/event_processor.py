e1b5870f9a236b33fd08b9a1a16cfabbde2e63c3
Joel Shprentz <jshprentz@his.com>
2020-11-06 20:38:14 -0500

    Process Meetup events into Wild Apricot events.

--- meetup2apricot/event_processor.py
+++ meetup2apricot/event_processor.py
@@ -2,27 +2,52 @@
 downloading Meetup photos for manual uploading to Wild Apricot, and by tracking
 events and photos already seen."""
 
+from .meetup_to_apricot_event_adaptor import MeetupToApricotEventAdaptor
+
 class EventProcessor:
 
     """Processes Meetup events into Wild Apricot events."""
 
-    def __init__(self, cutoff_time, known_events):
+    def __init__(self, cutoff_time, known_events, photo_cache, apricot_api):
         """Initialize with a cutoff time, a datetime before which events will
         be ignored; a dictionary of previously processed known events (indexed
-        by Meetup event ID)."""
+        by Meetup event ID); and a Wild Apricot API interface."""
         self.cutoff_time = cutoff_time
         self.known_events = known_events
+        self.photo_cache = photo_cache
+        self.apricot_api = apricot_api
 
     def process(self, meetup_event):
         """Process a meetup event."""
         if self.can_ignore_event(meetup_event):
             return
         photo_name = self.get_photo(meetup_event)
-        self.add_apricot_event(meetup_event, photo_name)
+        apricot_event_id = self.add_apricot_event(meetup_event, photo_name)
+        self.record_event(meetup_event, apricot_event_id)
 
     def can_ignore_event(self, meetup_event):
         """Return true if a Meetup event can be ignored; false otherwise."""
         return meetup_event.start_time < self.cutoff_time \
             or meetup_event.meetup_id in self.known_events
+
+    def get_photo(self, meetup_event):
+        """Get an available photo for a meetup event, if it hasn't been
+        downloaded before.  Return the photo name if it was ever downloaded or
+        None."""
+        return self.photo_cache.cache_photo(meetup_event)
+
+    def add_apricot_event(self, meetup_event, photo_name):
+        """Add the event to Wild Apricot."""
+        apricot_event = MeetupToApricotEventAdaptor(meetup_event, photo_name)
+        apricot_event_json = apricot_event.for_json()
+        return self.apricot_api.add_event(apricot_event_json)
+
+    def record_event(self, meetup_event, apricot_event_id):
+        """Record the known event to ignore in the future."""
+        self.known_events[meetup_event.meetup_id] = {
+            "wild_apricot_event": apricot_event_id,
+            "start_time": meetup_event.start_time
+            }
+
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 autoindent
--- tests/test_event_processor.py
+++ tests/test_event_processor.py
@@ -2,18 +2,42 @@
 
 from meetup2apricot.event_processor import EventProcessor
 from datetime import datetime
+from .sample_apricot_json import EXPECTED_FREE_PHOTO_NAME, EXPECTED_FREE_EVENT_JSON 
 import pytest
 
 
 CUTOFF_TIME = datetime.fromisoformat("2020-11-10 00:00 -05:00")
 
-KNOWN_EVENTS = {"274139316": "2020-11-13 19:00 -05:00"}
+KNOWN_EVENTS = {
+    "274139316": {
+        "wild_apricot_event": "4041234",
+        "start_time": "2020-11-13 19:00 -05:00"
+        }
+    }
+
+EXPECTED_APRICOT_EVENT_ID = "43210987"
 
 @pytest.fixture()
-def event_processor():
+def mock_photo_cache(mocker):
+    """Mock a photo cache, which implements a "cache_photo" method."""
+    mock_photo_cache = mocker.Mock()
+    mock_photo_cache.cache_photo = mocker.Mock(return_value="foo.jpg")
+    return mock_photo_cache
+
+@pytest.fixture()
+def mock_apricot_api(mocker):
+    """Mock a Wild Apricot API interface."""
+    mock_apricot_api = mocker.Mock()
+    mock_apricot_api.add_event = mocker.Mock(return_value=EXPECTED_APRICOT_EVENT_ID)
+    return mock_apricot_api
+
+@pytest.fixture()
+def event_processor(mock_photo_cache, mock_apricot_api):
     return EventProcessor(
         cutoff_time = CUTOFF_TIME,
-        known_events = KNOWN_EVENTS
+        known_events = KNOWN_EVENTS.copy(),
+        photo_cache = mock_photo_cache,
+        apricot_api = mock_apricot_api
         )
 
 def test_can_ignore_event_past(event_processor, free_meetup_event):
@@ -27,6 +51,39 @@ def test_can_ignore_event_future_new(event_processor, later_free_meetup_event):
 def test_can_ignore_event_seen(event_processor, paid_meetup_event):
     """Test that a previously seen event can be ignored."""
     assert event_processor.can_ignore_event(paid_meetup_event)
+
+def test_get_photo(event_processor, free_meetup_event, mock_photo_cache):
+    """Test getting a photo and it's Wild Apricot name."""
+    assert event_processor.get_photo(free_meetup_event) == "foo.jpg"
+    mock_photo_cache.cache_photo.assert_called_once_with(free_meetup_event)
+
+def test_add_apricot_event(event_processor, free_meetup_event, mock_apricot_api):
+    """Test adding a Wild Apricot event."""
+    assert event_processor.add_apricot_event(free_meetup_event, EXPECTED_FREE_PHOTO_NAME) == EXPECTED_APRICOT_EVENT_ID
+    mock_apricot_api.add_event.assert_called_once_with(EXPECTED_FREE_EVENT_JSON)
+
+def test_record_event(event_processor, free_meetup_event):
+    """Test recording a known event."""
+    assert free_meetup_event.meetup_id not in event_processor.known_events
+    event_processor.record_event(free_meetup_event, EXPECTED_APRICOT_EVENT_ID)
+    assert event_processor.known_events[free_meetup_event.meetup_id] == {
+            "wild_apricot_event": EXPECTED_APRICOT_EVENT_ID,
+            "start_time": free_meetup_event.start_time
+            }
+
+def test_process_skip(event_processor, free_meetup_event):
+    """Test processing an event to skip."""
+    event_processor.process(free_meetup_event)
+    assert free_meetup_event.meetup_id not in event_processor.known_events
+
+def test_process(event_processor, later_free_meetup_event):
+    """Test processing an event."""
+    event_processor.process(later_free_meetup_event)
+    assert event_processor.known_events[later_free_meetup_event.meetup_id] == {
+            "wild_apricot_event": EXPECTED_APRICOT_EVENT_ID,
+            "start_time": later_free_meetup_event.start_time
+            }
+
 
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 autoindent
