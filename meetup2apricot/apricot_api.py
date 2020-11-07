1183900722df6d3761470254ff3a42324d0a1c24
Joel Shprentz <jshprentz@his.com>
2020-11-07 16:56:43 -0500

    Remove some Xibo residue.

--- meetup2apricot/apricot_api.py
+++ meetup2apricot/apricot_api.py
@@ -1,7 +1,6 @@
 """Access Apricot API to insert events."""
 
 from .http_response_error import ApricotApiError
-from itertools import chain
 import logging
 
 
@@ -14,12 +13,11 @@ class ApricotApi:
     api_version = "v2.2"
     api_base_url = f"{api_url}/{api_version}"
 
-    def __init__(self, account_id, session, page_length):
-        """Initialize with a Wild Appricot account ID, an OAuth2 session to the
-        Apricot server, and a page length for paged retrievals."""
+    def __init__(self, account_id, session):
+        """Initialize with a Wild Appricot account ID and an OAuth2 session to
+        the Apricot server."""
         self.account_id = account_id
         self.session = session
-        self.page_length = page_length
 
     def get_response(self, url, **payload):
         """Request a URL and return the response."""
@@ -50,25 +48,6 @@ class ApricotApi:
         ApricotApiError.check_response_status(response)
         return response
 
-## May not need these
-
-    def get_json_pages(self, url, **payload):
-        """Return a generator of JSON pages requested from the URL."""
-        start = 0
-        response = self.get_json(
-                url, start=start, length=self.page_length, **payload)
-        yield response
-        while len(response) == self.page_length:
-            start += self.page_length
-            response = self.get_json(
-                    url, start=start, length=self.page_length, **payload)
-            yield response
-
-    def get_paged_json(self, url, **payload):
-        """Request a URL and return an iterable of all paged JSON results."""
-        json_pages = self.get_json_pages(url, **payload)
-        return chain.from_iterable(json_pages)
-
 ## Wild Apricot functions
 
     def get_event(self, event_id):
@@ -80,53 +59,6 @@ class ApricotApi:
         """Insert an event into Wild Apricot."""
         url = f"{self.api_base_url}/accounts/{self.account_id}/events"
         return self.post(url, json=event)
-
-## Xibo functions to adapt or delete
-
-    def get_about(self):
-        """Get the JSON representing the Apricot API version and
-        other "about" information."""
-        url = self.xibo_api_url_builder.about_url()
-        return self.get_json(url)
-
-    def get_xibo_api_version(self):
-        """Return the Apricot API version string."""
-        return self.get_about()["version"]
-
-    def get_datasets_by_code(self, dataset_code):
-        """Get a JSON list of metadata about a dataset,
-        searching by its code."""
-        url = self.xibo_api_url_builder.dataset_url()
-        return self.get_json(url, code=dataset_code)
-
-    def get_dataset_column_by_id(self, dataset_id):
-        """Get a JSON list of dataset column metadata,
-        searching by the dataset id."""
-        url = self.xibo_api_url_builder.dataset_column_url(dataset_id)
-        return self.get_paged_json(url)
-
-    def get_dataset_data_by_id(self, dataset_id):
-        """Get a JSON list of dataset data,
-        searching by its id."""
-        url = self.xibo_api_url_builder.dataset_data_url(dataset_id)
-        return self.get_paged_json(url)
-
-    def delete_dataset_data_by_id(self, dataset_id, row_id):
-        """Delete the dataset row."""
-        url = self.xibo_api_url_builder.dataset_data_row_url(
-                dataset_id, row_id)
-        return self.delete(url)
-
-    def insert_dataset_data(self, dataset_id, columns):
-        """Insert a row of data into the Apricot database."""
-        url = self.xibo_api_url_builder.dataset_data_url(dataset_id)
-        return self.post(url, **columns)
-
-    def update_dataset_data(self, dataset_id, row_id, columns):
-        """Update a row of data in the Apricot database."""
-        url = self.xibo_api_url_builder.dataset_data_row_url(
-                dataset_id, row_id)
-        return self.put(url, **columns)
 
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 autoindent
--- tests/test_apricot_api.py
+++ tests/test_apricot_api.py
@@ -8,9 +8,6 @@ import json
 import os
 import pytest
 
-SAMPLE_URL = "https://example.com/api"
-SAMPLE_XIBO_PAGE_LENGTH = 100
-
 
 @pytest.fixture()
 def apricot_api(apricot_session):
@@ -18,7 +15,7 @@ def apricot_api(apricot_session):
     account_id = os.getenv("APRICOT_ACCOUNT_ID")
     if not account_id:
         pytest.skip("Define environment variable APRICOT_ACCOUNT_ID")
-    return ApricotApi(account_id, apricot_session, SAMPLE_XIBO_PAGE_LENGTH)
+    return ApricotApi(account_id, apricot_session)
 
 def save_json(the_json, path):
     """Save JSON to a file."""
@@ -71,142 +68,11 @@ def test_add_event(mocker):
     """Save response from a "dataset data insert" request to Wild Apricot."""
     sample_event = { "name": "Sample Event", "start_date": "2020-11-02 19:00" }
     expected_url = "https://api.wildapricot.org/v2.2/accounts/12345/events"
-    apricot_api = ApricotApi("12345", None, SAMPLE_XIBO_PAGE_LENGTH)
+    apricot_api = ApricotApi("12345", None)
     apricot_api.post = mocker.Mock(return_value = 4567)
     response = apricot_api.add_event(sample_event)
     assert response == 4567
     apricot_api.post.assert_called_once_with(expected_url, json=sample_event)
-
-
-## These tests from Xibo testing must be adapted or deleted.
-
-@pytest.mark.skip(reason="Not ready to test")
-def test_get_datasets_by_code_response(module_file_path, apricot_api):
-    """Save response from a "dataset" request to Wild Apricot."""
-    dataset_code = os.getenv("EVENT_DATASET_CODE")
-    if not dataset_code:
-        pytest.skip("Define environment variable EVENT_DATASET_CODE")
-    apricot_json = apricot_api.get_datasets_by_code(dataset_code)
-    save_json(apricot_json, module_file_path)
-
-@pytest.mark.skip(reason="Not ready to test")
-def test_get_apricot_api_version(mocker):
-    """Testing getting the Wild Apricot API version number."""
-    apricot_api.get_about = mocker.Mock(return_value = SAMPLE_ABOUT_JSON)
-    assert apricot_api.get_apricot_api_version() == "1.8.12"
-
-@pytest.mark.skip(reason="Not ready to test")
-def test_get_dataset_column_response(module_file_path, apricot_api):
-    """Save response from a "dataset column" request to Wild Apricot."""
-    dataset_id = os.getenv("EVENT_DATASET_ID")
-    if not dataset_id:
-        pytest.skip("Define environment variable EVENT_DATASET_ID")
-    apricot_json = apricot_api.get_dataset_column_by_id(dataset_id)
-    save_json(list(apricot_json), module_file_path)
-
-@pytest.mark.skip(reason="Not ready to test")
-def test_get_response(module_file_path, apricot_api):
-    """Save response from a "dataset data" request to Wild Apricot."""
-    dataset_id = os.getenv("EVENT_DATASET_ID")
-    if not dataset_id:
-        pytest.skip("Define environment variable EVENT_DATASET_ID")
-    url = apricot_api_url_builder.dataset_data_url(dataset_id)
-    response = apricot_api.get_response(url, start = 100, length = 7)
-    save_response(response, module_file_path)
-
-@pytest.mark.skip(reason="Not ready to test")
-def test_get_dataset_data(module_file_path, apricot_api):
-    """Save JSON from a "dataset data" request to Wild Apricot."""
-    dataset_id = os.getenv("EVENT_DATASET_ID")
-    if not dataset_id:
-        pytest.skip("Define environment variable EVENT_DATASET_ID")
-    apricot_json = apricot_api.get_dataset_data_by_id(dataset_id)
-    save_json(list(apricot_json), module_file_path)
-
-@pytest.mark.skip(reason="Not ready to test")
-def test_delete_row_response(module_file_path, apricot_api):
-    """Save response from a "dataset data delete" request to Wild Apricot."""
-    row_id = os.getenv("DELETE_ROW_ID")
-    if not row_id:
-        pytest.skip("Environment variable DELETE_ROW_ID is not defined")
-    dataset_id = os.getenv("EVENT_DATASET_ID")
-    if not dataset_id:
-        pytest.skip("Define environment variable EVENT_DATASET_ID")
-    response = apricot_api.delete_dataset_data_by_id(dataset_id, row_id)
-    save_response(response, module_file_path)
-
-@pytest.mark.skip(reason="Not ready to test")
-def test_insert_dataset_data_response(module_file_path, apricot_api):
-    """Save response from a "dataset data insert" request to Wild Apricot."""
-    dataset_id = os.getenv("EVENT_DATASET_ID")
-    if not dataset_id:
-        pytest.skip("Define environment variable EVENT_DATASET_ID")
-    response = apricot_api.insert_dataset_data(dataset_id, SAMPLE_XIBO_EVENT_COLUMNS)
-    save_response(response, module_file_path)
-
-@pytest.mark.skip(reason="Not ready to test")
-def test_update_dataset_data_response(module_file_path, apricot_api):
-    """Save response from a "dataset data update" request to Wild Apricot."""
-    row_id = os.getenv("UPDATE_ROW_ID")
-    if not row_id:
-        pytest.skip("Environment variable UPDATE_ROW_ID is not defined")
-    dataset_id = os.getenv("EVENT_DATASET_ID")
-    if not dataset_id:
-        pytest.skip("Define environment variable EVENT_DATASET_ID")
-    response = apricot_api.update_dataset_data(dataset_id, row_id, SAMPLE_XIBO_EVENT_COLUMNS)
-    save_response(response, module_file_path)
-
-@pytest.mark.skip(reason="Not ready to test")
-def test_get_paged_json_0(mocker):
-    """Test getting 0 paged JSON results."""
-    apricot_api.get_json = mocker.Mock(return_value = SAMPLE_JSON_LIST_0)
-    results = apricot_api.get_paged_json(SAMPLE_URL)
-    assert list(results) == SAMPLE_JSON_LIST_0
-    apricot_api.get_json.assert_called_once_with(SAMPLE_URL, start = 0, length = SAMPLE_XIBO_PAGE_LENGTH)
-
-@pytest.mark.skip(reason="Not ready to test")
-def test_get_paged_json_1(mocker):
-    """Test getting 1 paged JSON result."""
-    apricot_api.get_json = mocker.Mock(return_value = SAMPLE_JSON_LIST_1)
-    results = apricot_api.get_paged_json(SAMPLE_URL)
-    assert list(results) == SAMPLE_JSON_LIST_1
-    apricot_api.get_json.assert_called_once_with(SAMPLE_URL, start = 0, length = SAMPLE_XIBO_PAGE_LENGTH)
-
-@pytest.mark.skip(reason="Not ready to test")
-def test_get_paged_json_2(mocker):
-    """Test getting 2 paged JSON results."""
-    apricot_api.get_json = mocker.Mock(return_value = SAMPLE_JSON_LIST_2)
-    results = apricot_api.get_paged_json(SAMPLE_URL)
-    assert list(results) == SAMPLE_JSON_LIST_2
-    apricot_api.get_json.assert_called_once_with(SAMPLE_URL, start = 0, length = SAMPLE_XIBO_PAGE_LENGTH)
-
-@pytest.mark.skip(reason="Not ready to test")
-def test_get_paged_json_3(mocker):
-    """Test getting 3 paged JSON results, requiring two pages."""
-    return_values = [SAMPLE_JSON_LIST_3, SAMPLE_JSON_LIST_0]
-    expected_calls = [
-        mocker.call(SAMPLE_URL, start = 0, length = SAMPLE_XIBO_PAGE_LENGTH),
-        mocker.call(SAMPLE_URL, start = SAMPLE_XIBO_PAGE_LENGTH, length = SAMPLE_XIBO_PAGE_LENGTH),
-        ]
-    apricot_api.get_json = mocker.Mock(side_effect = return_values)
-    results = apricot_api.get_paged_json(SAMPLE_URL)
-    assert list(results) == SAMPLE_JSON_LIST_3
-    assert apricot_api.get_json.call_args_list == expected_calls
-
-@pytest.mark.skip(reason="Not ready to test")
-def test_get_paged_json_4(mocker):
-    """Test getting 4 paged JSON results, requiring two pages."""
-    return_values = [SAMPLE_JSON_LIST_3, SAMPLE_JSON_LIST_1]
-    expected_calls = [
-        mocker.call(SAMPLE_URL, start = 0, length = SAMPLE_XIBO_PAGE_LENGTH),
-        mocker.call(SAMPLE_URL, start = SAMPLE_XIBO_PAGE_LENGTH, length = SAMPLE_XIBO_PAGE_LENGTH),
-        ]
-    apricot_api.get_json = mocker.Mock(side_effect = return_values)
-    results = apricot_api.get_paged_json(SAMPLE_URL)
-    assert list(results) == SAMPLE_JSON_LIST_3 + SAMPLE_JSON_LIST_1
-    assert apricot_api.get_json.call_args_list == expected_calls
-
-    
 
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 autoindent
