05e28ce7e0c3687fef929fc7451d9bd4cf6b0e5e
Joel Shprentz <jshprentz@his.com>
2020-11-08 07:05:48 -0500

    Add event registration types for new events.

--- meetup2apricot/event_processor.py
+++ meetup2apricot/event_processor.py
@@ -3,11 +3,16 @@ downloading Meetup photos for manual uploading to Wild Apricot, and by tracking
 events and photos already seen."""
 
 from .meetup_to_apricot_event_adaptor import MeetupToApricotEventAdaptor
+from .event_registration_type import make_apricot_registration_type, \
+    make_meetup_registration_type
 import pickle
+import logging
 
 class EventProcessor:
 
     """Processes Meetup events into Wild Apricot events."""
+
+    logger = logging.getLogger("EventProcessor")
 
     def __init__(self, cutoff_time, known_events, photo_cache, apricot_api,
             cache_path):
@@ -27,6 +32,7 @@ class EventProcessor:
             return
         photo_path = self.get_photo(meetup_event)
         apricot_event_id = self.add_apricot_event(meetup_event, photo_path)
+        self.add_event_registration_types(meetup_event, apricot_event_id)
         self.record_event(meetup_event, apricot_event_id)
 
     def can_ignore_event(self, meetup_event):
@@ -44,7 +50,31 @@ class EventProcessor:
         """Add the event to Wild Apricot."""
         apricot_event = MeetupToApricotEventAdaptor(meetup_event, photo_path)
         apricot_event_json = apricot_event.for_json()
-        return self.apricot_api.add_event(apricot_event_json)
+        apricot_event_id = self.apricot_api.add_event(apricot_event_json)
+        self.logger.info("add_apricot_event: meetup_id=%s apricot_id=%d "
+            "title=%r",
+            meetup_event.meetup_id, apricot_event_id, meetup_event.name)
+        return apricot_event_id
+
+    def add_event_registration_types(self, meetup_event, apricot_event_id):
+        meetup_count = meetup_event.yes_rsvp_count
+        if meetup_event.rsvp_limit:
+            apricot_count = meetup_event.rsvp_limit - meetup_count
+        else:
+            apricot_count = None
+        for reg_type in [
+            make_meetup_registration_type(
+                apricot_event_id, meetup_event.yes_rsvp_count),
+            make_apricot_registration_type(
+                apricot_event_id, apricot_count, meetup_event.fee_amount)]:
+            self.apricot_api.add_registration_type(reg_type.for_json())
+            if reg_type.maximum_registrants_count is None:
+                display_count = "unlimited"
+            else:
+                display_count = f"{reg_type.maximum_registrants_count:d}"
+            self.logger.info("add_event_registration_types: apricot_id=%d "
+                "maximum_registrants_count=%s price=%.2f name=%r",
+                apricot_event_id, display_count, reg_type.price, reg_type.name)
 
     def record_event(self, meetup_event, apricot_event_id):
         """Record the known event to ignore in the future."""
--- tests/test_event_processor.py
+++ tests/test_event_processor.py
@@ -16,9 +16,76 @@ KNOWN_EVENTS = {
         }
     }
 
-EXPECTED_APRICOT_EVENT_ID = "43210987"
+EXPECTED_APRICOT_EVENT_ID = 43210987
+
+EXPECTED_REGISTRATION_TYPE_ID = 76543
 
 CACHE_FILE_NAME = "event_processor.pickle"
+
+EXPECTED_MEETUP_RSVP_TYPE_FOR_FREE = {
+    'EventId': EXPECTED_APRICOT_EVENT_ID,
+    'Name': 'Meetup RSVP',
+    'IsEnabled': False,
+    'Description': 'RSVPs on Meetup',
+    'BasePrice': 0.0,
+    'GuestPrice': 0.0,
+    'Availability': 'Everyone',
+    'MaximumRegistrantsCount': 3,
+    'GuestRegistrationPolicy': 'CollectContactDetails',
+    'UnavailabilityPolicy': 'ShowDisabled',
+    'CancellationBehaviour': 'AllowUpToPeriodBeforeEvent',
+    'CancellationDaysBeforeEvent': 2,
+    'IsWaitlistEnabled': False
+    }
+
+EXPECTED_RSVP_TYPE_FOR_FREE = {
+    'EventId': EXPECTED_APRICOT_EVENT_ID,
+    'Name': 'RSVP',
+    'IsEnabled': True,
+    'Description': '',
+    'BasePrice': None,
+    'GuestPrice': None,
+    'Availability': 'Everyone',
+    'MaximumRegistrantsCount': None,
+    'GuestRegistrationPolicy': 'CollectContactDetails',
+    'UnavailabilityPolicy': 'ShowDisabled',
+    'CancellationBehaviour': 'AllowUpToPeriodBeforeEvent',
+    'CancellationDaysBeforeEvent': 2,
+    'IsWaitlistEnabled': False
+    }
+
+EXPECTED_MEETUP_RSVP_TYPE_FOR_PAID = {
+    'EventId': 7890,
+    'Name': 'Meetup RSVP',
+    'IsEnabled': False,
+    'Description': 'RSVPs on Meetup',
+    'BasePrice': 0.0,
+    'GuestPrice': 0.0,
+    'Availability': 'Everyone',
+    'MaximumRegistrantsCount': 2,
+    'GuestRegistrationPolicy': 'CollectContactDetails',
+    'UnavailabilityPolicy': 'ShowDisabled',
+    'CancellationBehaviour': 'AllowUpToPeriodBeforeEvent',
+    'CancellationDaysBeforeEvent': 2,
+    'IsWaitlistEnabled': False
+    }
+
+EXPECTED_RSVP_TYPE_FOR_PAID = {
+    'EventId': 7890,
+    'Name': 'RSVP',
+    'IsEnabled': True,
+    'Description': '',
+    'BasePrice': 20.0,
+    'GuestPrice': 20.0,
+    'Availability': 'Everyone',
+    'MaximumRegistrantsCount': 4,
+    'GuestRegistrationPolicy': 'CollectContactDetails',
+    'UnavailabilityPolicy': 'ShowDisabled',
+    'CancellationBehaviour': 'AllowUpToPeriodBeforeEvent',
+    'CancellationDaysBeforeEvent': 2,
+    'IsWaitlistEnabled': False
+    }
+
 
 @pytest.fixture()
 def mock_photo_cache(mocker):
@@ -32,6 +99,7 @@ def mock_apricot_api(mocker):
     """Mock a Wild Apricot API interface."""
     mock_apricot_api = mocker.Mock()
     mock_apricot_api.add_event = mocker.Mock(return_value=EXPECTED_APRICOT_EVENT_ID)
+    mock_apricot_api.add_registration_type = mocker.Mock(return_value=EXPECTED_REGISTRATION_TYPE_ID)
     return mock_apricot_api
 
 @pytest.fixture()
@@ -66,6 +134,26 @@ def test_add_apricot_event(event_processor, free_meetup_event, mock_apricot_api)
     assert event_processor.add_apricot_event(free_meetup_event, EXPECTED_FREE_PHOTO_PATH) == EXPECTED_APRICOT_EVENT_ID
     mock_apricot_api.add_event.assert_called_once_with(EXPECTED_FREE_EVENT_JSON)
 
+def test_add_event_registration_types_unlimited(event_processor,
+        free_meetup_event, mock_apricot_api, mocker):
+    """Test adding registration types for a Wild Apricot event with unlimited
+    capacity."""
+    expected_calls = [
+        mocker.call(EXPECTED_MEETUP_RSVP_TYPE_FOR_FREE ),
+        mocker.call(EXPECTED_RSVP_TYPE_FOR_FREE )]
+    event_processor.add_event_registration_types(free_meetup_event, EXPECTED_APRICOT_EVENT_ID)
+    mock_apricot_api.add_registration_type.assert_has_calls(expected_calls)
+
+def test_add_event_registration_types_limited(event_processor,
+        paid_meetup_event, mock_apricot_api, mocker):
+    """Test adding registration types for a Wild Apricot event with limited
+    capacity."""
+    expected_calls = [
+        mocker.call(EXPECTED_MEETUP_RSVP_TYPE_FOR_PAID ),
+        mocker.call(EXPECTED_RSVP_TYPE_FOR_PAID )]
+    event_processor.add_event_registration_types(paid_meetup_event, 7890)
+    mock_apricot_api.add_registration_type.assert_has_calls(expected_calls)
+
 def test_record_event(event_processor, free_meetup_event):
     """Test recording a known event."""
     assert free_meetup_event.meetup_id not in event_processor.known_events
@@ -80,13 +168,17 @@ def test_process_skip(event_processor, free_meetup_event):
     event_processor.process(free_meetup_event)
     assert free_meetup_event.meetup_id not in event_processor.known_events
 
-def test_process(event_processor, later_free_meetup_event):
+def test_process(event_processor, later_free_meetup_event, mock_apricot_api, mocker):
     """Test processing an event."""
+    expected_calls = [
+        mocker.call(EXPECTED_MEETUP_RSVP_TYPE_FOR_FREE ),
+        mocker.call(EXPECTED_RSVP_TYPE_FOR_FREE )]
     event_processor.process(later_free_meetup_event)
     assert event_processor.known_events[later_free_meetup_event.meetup_id] == {
             "wild_apricot_event": EXPECTED_APRICOT_EVENT_ID,
             "start_time": later_free_meetup_event.start_time
             }
+    mock_apricot_api.add_registration_type.assert_has_calls(expected_calls)
 
 def test_persist(event_processor, tmp_path):
     """Test persisting the event processor."""
