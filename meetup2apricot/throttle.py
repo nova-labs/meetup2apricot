9dc875350548417e20dc040974f8cbfaeb9be549
Joel Shprentz <jshprentz@his.com>
2020-11-14 15:35:41 -0500

    Make throttles with a configured utilization factor.

--- meetup2apricot/throttle.py
+++ meetup2apricot/throttle.py
@@ -1,5 +1,12 @@
+"""Activity throttles typically used to keep API call rates below allowed
+limits."""
+
+import logging
 import time
 from collections import deque
+
+MIN_UTILIZATION = 0.05
+MAX_UTILIZATION = 0.95
 
 
 class Throttle:
@@ -10,6 +17,7 @@ class Throttle:
         """Initialize with a rate (integer number of events) and a time span
         (seconds)."""
         self.time_span = time_span
+        self.rate = rate
         self.ready_times = deque([0] * rate, rate)
 
     def is_ready(self, current_time=None):
@@ -35,6 +43,30 @@ class Throttle:
         """Throttle an event, waiting until the next ready time."""
         self.wait(current_time)
         self.event()
+
+
+def make_throttle(rate, time_span, utilization_factor, purpose):
+    """Return a new throttle given a rate (integer number of events), a time
+    span (seconds), a utilization factor (number between 0.05 and 0.95), and a
+    purpose (for logging)."""
+    logger = logging.getLogger("make_throttle")
+    if not (MIN_UTILIZATION <= utilization_factor <= MAX_UTILIZATION):
+        logger.warning(
+            "utilization_factor must be between %0.2f and %0.2f. "
+            "purpose=%r rate=%d time_span=%d utilization_factor=%f",
+            MIN_UTILIZATION,
+            MAX_UTILIZATION,
+            purpose,
+            rate,
+            time_span,
+            utilization_factor,
+        )
+        utilization_factor = max(
+            MIN_UTILIZATION, min(MAX_UTILIZATION, utilization_factor)
+        )
+    allocated_rate = round(rate * utilization_factor)
+    logger.debug("purpose=%r rate=%d time_span=%d", purpose, allocated_rate, time_span)
+    return Throttle(allocated_rate, time_span)
 
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 autoindent
