b35bcf66e03b9f26a6ae1e90c16b3a7cff06450a
Joel Shprentz <jshprentz@his.com>
2021-02-19 17:45:14 -0500

    Transfer only selected events, if specified.

--- meetup2apricot/initial_data_loader.py
+++ meetup2apricot/initial_data_loader.py
@@ -38,15 +38,17 @@ class InitialDataLoader:
         self,
         meetup_api,
         apricot_api,
+        transfer_meetup_ids,
         event_mapping_provider,
         photo_urls_provider,
         enter_initial_data_scope,
     ):
-        """Initialize with Meetup and Wild Apricot APIs and functions to
-        provide cached event and photo data and to enter the initial data
-        scope."""
+        """Initialize with Meetup and Wild Apricot APIs, a list of Meetup IDs
+        of events to transfer, and functions to provide cached event and photo
+        data and to enter the initial data scope."""
         self.meetup_api = meetup_api
         self.apricot_api = apricot_api
+        self.transfer_meetup_ids = frozenset(transfer_meetup_ids)
         self.event_mapping_provider = event_mapping_provider
         self.photo_urls_provider = photo_urls_provider
         self.enter_initial_data_scope = enter_initial_data_scope
@@ -54,11 +56,13 @@ class InitialDataLoader:
     def run(self):
         """Run the Meetup to Wild Apricot conversion."""
         upcoming_meetup_events = self.retreive_upcoming_meetup_events()
+        transfer_meetup_events = self.select_events_to_transfer(upcoming_meetup_events)
         membership_levels = self.retrieve_membership_levels()
         event_mapping = self.event_mapping_provider()
         photo_urls_to_paths = self.photo_urls_provider()
         self.convert_events(
             upcoming_meetup_events,
+            transfer_meetup_events,
             membership_levels,
             photo_urls_to_paths,
             event_mapping,
@@ -69,6 +73,17 @@ class InitialDataLoader:
         json_events = self.meetup_api.retrieve_events_json()
         return [MeetupEvent(event) for event in json_events]
 
+    def select_events_to_transfer(self, upcoming_meetup_events):
+        """Return a list of Meetup events to transfer to Wild Apricot."""
+        if self.transfer_meetup_ids:
+            return [
+                event
+                for event in upcoming_meetup_events
+                if event.meetup_id in self.transfer_meetup_ids
+            ]
+        else:
+            return upcoming_meetup_events
+
     def retrieve_membership_levels(self):
         """Return a list of Wild Apricot membership levels."""
         return self.apricot_api.get_membership_levels()
@@ -76,6 +91,7 @@ class InitialDataLoader:
     def convert_events(
         self,
         upcoming_meetup_events,
+        transfer_meetup_events,
         membership_levels,
         photo_urls_to_paths,
         event_mapping,
@@ -83,7 +99,7 @@ class InitialDataLoader:
         """Convert Meetup events to Wild Apricot events using some initial data."""
         initial_data_scope = InitialDataScope(
             upcoming_meetup_events,
-            upcoming_meetup_events,
+            transfer_meetup_events,
             membership_levels,
             photo_urls_to_paths,
             event_mapping,
