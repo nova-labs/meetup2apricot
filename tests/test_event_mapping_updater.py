55a8204cc357e74449730f4e0ac1dbb0ac766f08
Joel Shprentz <jshprentz@his.com>
2021-03-24 12:10:38 -0400

    Refactor to use fake Meetup event retriever instead of mock.

--- tests/test_event_mapping_updater.py
+++ tests/test_event_mapping_updater.py
@@ -8,44 +8,60 @@ SAMPLE_EARLIEST_START_TIME = datetime.fromisoformat("2020-11-06 00:00 -05:00")
 
 MEETUP_ID_1 = "1234"
 MEETUP_ID_2 = "3456"
-MEETUP_ID_3 = "5678"
 FREE_MEETUP_ID = "pfsbvrybcpbmb"
+PAID_MEETUP_ID = "274139316"
+PREV_FREE_MEETUP_ID = "abcde"
 
 APRICOT_DATA_1 = {
-    "wild_apricot_event": 9876,
+    "wild_apricot_event": 4321,
     "start_time": datetime.fromisoformat("2020-11-01 19:30 -05:00"),
 }
 
 APRICOT_DATA_2 = {
     "wild_apricot_event": 6543,
-    "start_time": datetime.fromisoformat("2020-11-09 21:00 -05:00"),
+    "start_time": datetime.fromisoformat("2020-11-07 18:00 -05:00"),
 }
 
-APRICOT_DATA_3 = {
-    "wild_apricot_event": 3210,
-    "start_time": datetime.fromisoformat("2020-11-12 18:00 -05:00"),
+FREE_EVENT_APRICOT_DATA = {
+    "wild_apricot_event": 8765,
+    "start_time": datetime.fromisoformat("2020-11-09 21:00 -05:00"),
 }
 
 SAMPLE_EVENT_MAPPING = {
     MEETUP_ID_1: APRICOT_DATA_1,
     MEETUP_ID_2: APRICOT_DATA_2,
-    MEETUP_ID_3: APRICOT_DATA_3,
+    FREE_MEETUP_ID: FREE_EVENT_APRICOT_DATA,
 }
 
 
-@pytest.fixture
-def mock_meetup_event_retriever(mocker, free_meetup_event):
-    """Return a mock Meetup event retriever, which returns the free event."""
-    mock_retriever = mocker.Mock()
-    mock_retriever.get_event = mocker.Mock(side_effect=[free_meetup_event, None])
-    return mock_retriever
+class MeetupEventRetrieverFake:
+
+    """Returns free and paid Meetup events from several IDs."""
+
+    def __init__(self, free_meetup_event, paid_meetup_event):
+        """Initialize with free and paid Meetup events."""
+        self.events = {
+            free_meetup_event.meetup_id: free_meetup_event,
+            PREV_FREE_MEETUP_ID: free_meetup_event,
+            paid_meetup_event.meetup_id: paid_meetup_event,
+        }
+
+    def get_event(self, meetup_event_id):
+        """Fake getting and returning a Meetup event by its ID."""
+        return self.events.get(meetup_event_id, None)
 
 
 @pytest.fixture
-def event_mapping_updater(mock_meetup_event_retriever):
+def fake_meetup_event_retriever(free_meetup_event, paid_meetup_event):
+    """Return a fake Meetup event retriever for testing."""
+    return MeetupEventRetrieverFake(free_meetup_event, paid_meetup_event)
+
+
+@pytest.fixture
+def event_mapping_updater(fake_meetup_event_retriever):
     """Return an event mapping updater for testing."""
     return EventMappingUpdater(
-        mock_meetup_event_retriever,
+        fake_meetup_event_retriever,
         SAMPLE_EARLIEST_START_TIME,
         [],  # TODO provide skip Meetup event IDs
     )
@@ -63,19 +79,21 @@ def test_is_timely_late(event_mapping_updater):
 
 def test_update_meetup_id(event_mapping_updater):
     """Test getting an updated meetup ID."""
-    assert event_mapping_updater.update_meetup_id(MEETUP_ID_1) == FREE_MEETUP_ID
+    assert event_mapping_updater.update_meetup_id(PREV_FREE_MEETUP_ID) == FREE_MEETUP_ID
 
 
-def test_update_meetup_id_none(
-    event_mapping_updater, mock_meetup_event_retriever, mocker
-):
+def test_update_meetup_id_unchanged(event_mapping_updater):
+    """Test getting an unchanged meetup ID."""
+    assert event_mapping_updater.update_meetup_id(PAID_MEETUP_ID) == PAID_MEETUP_ID
+
+
+def test_update_meetup_id_none(event_mapping_updater):
     """Test getting an updated meetup ID when none is available."""
-    mock_meetup_event_retriever.get_event = mocker.Mock(return_value=None)
     assert event_mapping_updater.update_meetup_id(MEETUP_ID_2) == None
 
 
 def test_update_event_mapping(event_mapping_updater):
-    expected_mapping = {FREE_MEETUP_ID: APRICOT_DATA_2}
+    expected_mapping = {FREE_MEETUP_ID: FREE_EVENT_APRICOT_DATA}
     assert (
         event_mapping_updater.update_event_mapping(SAMPLE_EVENT_MAPPING)
         == expected_mapping
